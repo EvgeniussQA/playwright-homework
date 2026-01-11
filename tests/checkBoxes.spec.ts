f2030adf92fe27bb13e013c412b785217680937b
Evgeniy Puysho <evg.puisho@gmail.com>
2026-01-11 23:24:21 +0300

    Checkboxes:all 3 tests are done

--- tests/checkBoxes.spec.ts
+++ tests/checkBoxes.spec.ts
@@ -42,21 +42,40 @@ test("Select all specialties", async ({ page }) => {
     .getByRole("row", { name: vetName })
     .getByRole("button", { name: "Edit Vet" });
   const specialitiesDropdown = page.locator(".selected-specialties");
-  const radiologyCheckbox = page.getByRole("checkbox", {
-    name: "radiology",
-  });
-  const surgeryCheckbox = page.getByRole("checkbox", { name: "surgery" });
-  const dentistryCheckbox = page.getByRole("checkbox", {
-    name: "dentistry",
-  });
+  await editVetButton.click();
+  await expect(specialitiesDropdown).toHaveText("surgery");
+  await specialitiesDropdown.click();
 
-await editVetButton.click();
-await expect(specialitiesDropdown).toHaveText("surgery");
-await specialitiesDropdown.click();
+  const allBoxes = page.getByRole("checkbox");
+  for (const box of await allBoxes.all()) {
+    await box.check({ force: true });
+    expect(await box.isChecked()).toBeTruthy();
+  }
+  await expect(specialitiesDropdown).toHaveText(
+    "surgery, radiology, dentistry"
+  );
+});
 
+test("Deselect all specialties", async ({ page }) => {
+  const vetName = "Linda Douglas";
+  const editVetButton = page
+    .getByRole("row", { name: vetName })
+    .getByRole("button", { name: "Edit Vet" });
+  const specialitiesDropdown = page.locator(".selected-specialties");
 
-//   const allBoxes = page.getByRole('checkbox')
-// for(const box of await allBoxes.all()){
-// await box.uncheck({force: true})
-// expect (await box.isChecked()).toBeFalsy()
+  await editVetButton.click();
+  // I wasn't sure about the order, since requirements state "surgery, dentistry"
+  // but the UI displays "dentistry, surgery". I used the actual UI order.
+  // As a workaround for order-independent validation, toContainText could be used:
+  // await expect(specialitiesDropdown).toContainText("surgery");
+  // await expect(specialitiesDropdown).toContainText("dentistry");
+  await expect(specialitiesDropdown).toHaveText("dentistry, surgery");
+  await specialitiesDropdown.click();
+
+  const allBoxes = page.getByRole("checkbox");
+  for (const box of await allBoxes.all()) {
+    await box.uncheck({ force: true });
+    expect(await box.isChecked()).toBeFalsy();
+  }
+  await expect(specialitiesDropdown).toBeEmpty();
 });
